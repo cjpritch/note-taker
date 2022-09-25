6899a7c8c9cbe0b623e8c525593d9737384da9c2
Carly Pritchard <cpritch616@gmail.com>
2022-09-25 15:53:29 -0500

    functions to add and delete data from the json db

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-const notes = require ('./db/db.json')
+const notes = require('./db/db.json');
+const { createsNote, deletesNote} = require('./lib/createDelete');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,6 +9,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+// routes go here
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
