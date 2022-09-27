6ce0cfc73b61195edffe05ce6de0d4d3072e7ce6
Carly Pritchard <cpritch616@gmail.com>
2022-09-27 15:53:33 -0500

    fix bug in db.json

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-const notes = require('./db/db.json');
-const { createNote, deleteNote} = require('./lib/createDelete');
+const notes = require ('./db/db.json')
+const { createNote, deleteNote } = require('./lib/createDelete');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,33 +11,34 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Routes
+
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
-  });
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+    });
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes)
-  });
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-  });
+    res.json(notes)
+    });
 
 app.post('/api/notes', (req, res) => {
-req.body.id = notes.length.toString();
-let note = createNote(req.body, notes);
-res.json(notes);
-  });    
+  req.body.id = notes.length.toString();
+  let note = createNote(req.body, notes);
+  res.json(notes);
+    });    
 
 app.delete('/api/notes/:id', (req, res) => {
-  deleteNote(notes, req.params.id);
-  res.json(notes);
+    deleteNote(notes, req.params.id);
+    res.json(notes);
 });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+    });
 
 app.use((req, res) => {
   res.status(404).end();
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  console.log(`Server running on port ${PORT}`);
+});
