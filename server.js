d13273b62102509f145da6bcab20b4fcf72347af
Carly Pritchard <cpritch616@gmail.com>
2022-09-27 14:12:45 -0500

    complete api/html routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const notes = require('./db/db.json');
-const { createsNote, deletesNote} = require('./lib/createDelete');
+const { createNote, deleteNote} = require('./lib/createDelete');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,7 +10,33 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// routes go here
+// Routes
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+  });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+app.post('/api/notes', (req, res) => {
+req.body.id = notes.length.toString();
+let note = createNote(req.body, notes);
+res.json(notes);
+  });    
+
+app.delete('/api/notes/:id', (req, res) => {
+  deleteNote(notes, req.params.id);
+  res.json(notes);
+});
+
+app.use((req, res) => {
+  res.status(404).end();
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
